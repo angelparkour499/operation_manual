b94d8e7ae52e2d7e8361004013e9d6e1963ed91c
Angel <angel_epa_luses@hotmail.com>
2019-12-27 12:48:00 -0500

    typography

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*const options = document.getElementsByClassName("option")
+const options = document.getElementsByClassName("option")
 const select = document.getElementById("select-id")
 const select_options = document.getElementById("select-options")
 
@@ -39,46 +39,6 @@ function select_disable(e) {
     }
 
 
-    Multiple select page*/
 
 
-
-    const options = document.getElementsByClassName("option")
-const select = document.getElementById("select-id")
-const select_options = document.getElementById("select-options")
-
-function get_value() {
-  var value = document.getElementById("select-id").value
-  if (value) {
-  alert(value)
-  }
-  else {
-      select.classList.add("required-select")
-  }
-}
-
-
-for(var index = 0; index < options.length; index ++) {
-    options[index].addEventListener("click", handle_value)
-}
-
-function handle_value(e) {
-    var target_text = e.target.innerHTML
-    var target_value = e.target.value
-    select.innerHTML = target_text
-    select.value = target_value
-    for(var i = 0; i < options.length; i++) {
-        options[i].classList.remove("selected-option")
-    }
-    e.target.classList.add("selected-option")
-    select.classList.remove("required-select")
-}
-select.addEventListener("focusin", select_enable) 
-select.addEventListener("focusout", select_disable) 
-
-function select_enable(e) {
-select_options.classList.add("select-options-open")
-}
-function select_disable(e) {
-    select_options.classList.remove("select-options-open")
-    }
+ 

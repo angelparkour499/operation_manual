0ff1d7b5fbf32c069273a96cab83865c2cf6e8e8
Angel <angel_epa_luses@hotmail.com>
2019-12-30 12:05:17 -0500

    input field

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const options = document.getElementsByClassName("option")
 const select = document.getElementById("select-id")
 const select_options = document.getElementById("select-options")
+const button = document.getElementById("button")
+const secondary_button = document.getElementById("secondary-button")
+
+document.getElementById("button-switch").addEventListener("click", change_button)
+document.getElementById("secondary-button-switch").addEventListener("click", change_secondary_button)
 
 function get_value() {
   var value = document.getElementById("select-id").value
@@ -11,6 +16,31 @@ function get_value() {
       select.classList.add("required-select")
   }
 }
+var toggle_button = 1
+function change_button() {
+if (toggle_button) {
+    button.disabled = true
+    toggle_button = 0
+}
+else {
+    button.disabled = false
+    toggle_button = 1
+}
+
+    }
+
+    var toggle_button_2 = 1
+function change_secondary_button() {
+if (toggle_button_2) {
+    secondary_button.disabled = true
+    toggle_button_2 = 0
+}
+else {
+    secondary_button.disabled = false
+    toggle_button_2 = 1
+}
+
+    }
 
 
 for(var index = 0; index < options.length; index ++) {

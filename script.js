0e46a240f67bdff2d24bca18d2b9f0008f18f322
Angel <angel_epa_luses@hotmail.com>
2020-01-02 14:23:31 -0500

    input field

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const select = document.getElementById("select-id")
 const select_options = document.getElementById("select-options")
 const button = document.getElementById("button")
 const secondary_button = document.getElementById("secondary-button")
+const checkbox_1 = document.getElementById("check")
+const password = document.getElementById("password")
+const inputs = document.querySelectorAll("[data-input]")
+const input_switch = document.getElementById("input-switch")
 
 document.getElementById("button-switch").addEventListener("click", change_button)
 document.getElementById("secondary-button-switch").addEventListener("click", change_secondary_button)
@@ -82,4 +86,49 @@ else {
     return
 }
 }
- 
+
+ checkbox_1.addEventListener("click", show_password)
+
+ function show_password(e) {
+     if(password.type === "password") {
+         password.type = "text"
+         e.target.innerText = "hide"
+     }
+     else {
+         password.type = "password"
+         e.target.innerText = "show"
+     }
+ }
+var input_content;
+ (function(e) {
+for(var i = 0;i < inputs.length; i++) {
+    if(inputs[i].disabled) {
+        input_content = inputs[i]
+        inputs[i].classList.add("disable-input")
+        inputs[i].placeholder = "Disable"
+    }
+}
+ })()
+ input_switch.addEventListener("click", toggle_enable)
+var toggle_input = 0
+ function toggle_enable(e) {
+     if(toggle_input) {
+        for(var i = 0;i < inputs.length; i++) {
+                inputs[i].classList.add("disable-input")
+                inputs[i].placeholder = "Disable"
+                inputs[i].innerText = ""
+                inputs[i].disabled = true
+                inputs[i].value = ""
+        }
+        toggle_input = 0
+     }
+     else {
+        for(var i = 0;i < inputs.length; i++) {
+                inputs[i].classList.remove("disable-input")
+                inputs[i].placeholder = ""
+                inputs[i].innerHTML = input_content[i]
+                inputs[i].disabled = false
+            }
+        toggle_input = 1
+     }
+ }
